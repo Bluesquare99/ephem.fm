0c8f72a157933673e70d6ab2920fe6a248a0f337
Max McCready <msmccready@gmail.com>
2022-01-22 22:35:45 -0600

    added weather updates

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,30 @@
 import * as model from "./model.js";
+import { MINUTES } from "./config.js";
 import weatherView from "./views/weatherView.js";
 
-const controlWeather = async function (station) {
-  // Retrieves weather data from model
-  const weather = await model.retrieveWeather(station);
+const controlStation = async function (station) {
+  try {
+    // Updates weather given station
+    const weather = await model.updateWeather(station);
+    model.state.station = station;
+    model.state.weather = weather;
 
-  // Renders weather
-  weatherView.render(model.stations[station], weather);
+    // Periodically checks if weather data is current
+    //  If not, updates weather
+    async function execute1() {
+      while (true) {
+        await new Promise((resolve) => setTimeout(resolve, 2 * MINUTES));
+        await model.checkWeather(station);
+      }
+    }
+
+    execute1();
+
+    // Renders weather
+    weatherView.render(model.stations[station], weather);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-controlWeather("fbi");
-model.updateWeather("fbi");
+controlStation("kutx");
--- src/js/model.js
+++ src/js/model.js
@@ -21,24 +21,13 @@ export const stations = {
   },
 };
 
-// export const updateStation = function (station) {
-//   state.station = station;
-//   station.weather = retrieveWeather(station);
-// };
-
-export const retrieveWeather = async function (station) {
+export const updateWeather = async function (station) {
   try {
     const [lat, lng] = stations[station].coordinates;
     const url = `${API_WEATHER_URL}lat=${lat}&lon=${lng}&appid=${API_WEATHER_KEY}&units=imperial`;
 
     const data = await fetch(url);
     const weather = await data.json();
-    state.station = station;
-    state.weather = weather;
-
-    // add an updateState method
-    // const { speed, deg, gust } = wind;
-    // state.weather.wind = { speed, deg, gust };
 
     return weather;
   } catch (err) {
@@ -46,27 +35,28 @@ export const retrieveWeather = async function (station) {
   }
 };
 
-// export const updateState();
-
-export const updateWeather = function (station) {
-  setInterval(updateWeatherData(state, station), 1 * MINUTES);
-};
-
-export const updateWeatherData = async function (state, station) {
+export const checkWeather = async function (station) {
   try {
-    const prev = state;
-    const cur = await retrieveWeather(station);
+    console.log("Checking weather!");
+    const needsUpdate = false;
 
+    const prev = state;
     console.log("prev", prev.weather);
+    const cur = await updateWeather(state.station);
     console.log("cur", cur);
-    if (prev != cur) console.log(`Something's fishy!`);
-    else console.log("All good here");
-    // // Update all at once
-    // if (prev.speed !== speed) state.weather.wind.speed = speed;
-    // if (prev.deg !== deg) state.weather.wind.deg = deg;
-    // if (prev.gust !== gust) state.weather.wind.gust = gust;
-    // console.log("Wind data updated!");
+
+    if (
+      prev.weather.wind.speed !== cur.wind.speed ||
+      prev.weather.wind.dir !== cur.wind.dir ||
+      prev.weather.main.temp !== cur.main.temp ||
+      prev.weather.weather[0].description !== cur.weather[0].description
+    ) {
+      console.log("Changing now");
+      needsUpdate = true;
+    } else console.log(`They were same at ${Date.now()}`);
+
+    return needsUpdate;
   } catch (err) {
-    console.error("Trouble updating error!");
+    console.error(err);
   }
 };
