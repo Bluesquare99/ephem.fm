9a61b23b5445f74eed3760f5e1d91131c8fc9dfb
Max McCready <msmccready@gmail.com>
2022-01-25 22:18:25 -0600

    expanded gitignore

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+export const API_WEATHER_URL =
+  "https://api.openweathermap.org/data/2.5/weather?";
+export const API_WEATHER_KEY = "effae31f7bfb25c7219c16a6f16eab0f";
+export const MINUTES = 1000 * 60;
